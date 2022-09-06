cb89e796cb0cb207e0033364ceceff54ef71dca0
Anandkumar070 <109938222+Anandkumar070@users.noreply.github.com>
2022-09-06 12:26:30 +0530

    First Commit

---
+++ HandlingUserAuthentication/src/main/java/com/UserAuthentication.java
@@ -0,0 +1,16 @@
+package com;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "anand";
+	}
+	public String getEmail() {
+		return "anand@gmail.com";
+	}
+	public int getPhnNum() {
+		return 123456789;
+	}
+	public String getPassword() {
+		return "anand@123";
+	}
+}
